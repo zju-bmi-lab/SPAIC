a7bd3589effc574d6f1464978757e7371d52fe30
hongchaofei <hongchf@zhejianglab.com>
2022-01-24 10:00:27 +0800

    0.2.0.7.7
    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+# python -m build
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+        name='SPAIC',
+        version='0.2.0.7.7',
+        description='This is a test of setup for SPAIC',
+        author='zjlab',
+        packages=find_packages(),
+        include_package_data=False,
+        install_requires=[
+                                'torch>=1.5.0',
+                                'matplotlib',
+                                'numpy',
+                                'tqdm',
+                                'scipy',
+                                'tables',
+                           ],
+        python_requires='>=3.6'
+)
